9071ebf123a4ea1d39d75a2472cd504fdb1c9f42
Aryan Raj <23it3013@rgipt.ac.in>
2026-04-08 00:24:46 +0530

    Update api.js

--- todo-frontend/src/utils/api.js
+++ todo-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-app-uqvi.onrender.com",
+  baseURL: "https://task-manager-app-uqvi.onrender.com/api",
 });
 
 // Attach token automatically
