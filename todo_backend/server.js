52325fc2f37dd6e092de280f57c391cd351f47b9
Aryan Raj <23it3013@rgipt.ac.in>
2026-04-08 02:35:27 +0530

    Update CORS origin for production deployment

--- todo_backend/server.js
+++ todo_backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://threetask-manager-app-4.onrender.com",
   credentials: true
 }));
 
